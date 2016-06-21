9ae62ea3c94d8bec951b738c19d8d59f10e3f745
scottcrossen <scottcrossen42@gmail.com>
2016-06-21 12:55:25 -0600

    added color to debug

--- debugger.h
+++ debugger.h
@@ -13,15 +13,15 @@ class Debugger{
   Debugger(bool turned_on){
     this->turned_on=turned_on;
     last_flag=false;
-    if(turned_on) cout << "Debug: Initialized" <<endl;
+    if(turned_on) cout << "\033[1;34mDebug:\033[0m Initialized" <<endl;
   }
   ~Debugger(){
-    if(turned_on) cout << "Debug: Terminated" <<endl;
+    if(turned_on) cout << "\033[1;34mDebug:\033[0m Terminated" <<endl;
   }
   void flag(int add_flag){
     if(turned_on){
       flags.push_back(add_flag);
-      if(!(last_flag)) cout << "Debug: Flags encountered:\t"<< add_flag;
+      if(!(last_flag)) cout << "\033[1;34mDebug:\033[0m Flags encountered:\t"<< add_flag;
       else cout << " " << add_flag;
       last_flag=true;
     }
@@ -29,7 +29,7 @@ class Debugger{
   void flags_display(){
     if(turned_on){
       if(last_flag){ cout << endl; last_flag=false;}
-      cout << "Debug: Displaying flags found:\t";
+      cout << "\033[1;34mDebug:\033[0m Displaying flags found:\t";
       for(unsigned i=0; i < flags.size(); i++)
 	cout << flags[i] << " ";
       cout << endl;
@@ -38,14 +38,14 @@ class Debugger{
   void output(string current_output){
     if(turned_on){
       if(last_flag){cout << endl; last_flag=false;}
-      cout << "Debug: Status line:\t\t" << current_output << endl;
+      cout << "\033[1;34mDebug:\033[0m Status line:\t\t" << current_output << endl;
     }
   }
   void flags_clear(){
     if(turned_on){
       if(last_flag){ cout << endl; last_flag=false;}
       for (unsigned i=0; i < flags.size(); i++) flags.pop_back();
-      cout << "Debug: Flags cleared." << endl;
+      cout << "\033[1;34mDebug:\033[0m Flags cleared." << endl;
     }
   }
   bool encountered(int current_flag){
@@ -53,19 +53,19 @@ class Debugger{
       if(last_flag){ cout << endl; last_flag=false;}
       for (unsigned i=0; i < flags.size(); i++)
 	if(flags[i]==current_flag){
-	  cout << "Debug: Flag 1 conditional met:\tTrue"<< endl;
+	  cout << "\033[1;34mDebug:\033[0m Flag 1 conditional met:\tTrue"<< endl;
 	  return true;
 	}
-      cout << "Debug: Flag 1 conditional met:\tFalse"<< endl;
+      cout << "\033[1;34mDebug:\033[0m Flag 1 conditional met:\tFalse"<< endl;
       return false;
     } else return false;
   }
   void abort(bool conditional_met){
     if(turned_on){
       if(conditional_met){
-	cout << "Debug: Aborting. Unknown flag." << endl;
+	cout << "\033[1;34mDebug:\033[0m Aborting. Unknown flag." << endl;
 	flags_display();
-	if(turned_on) cout << "Debug: Terminated" <<endl;
+	if(turned_on) cout << "\033[1;34mDebug:\033[0m Terminated" <<endl;
 	exit(0);
       }
     }
@@ -73,24 +73,24 @@ class Debugger{
   void abort(bool conditional_met, int current_flag){
     if(turned_on){
       if(conditional_met){
-	cout << "Debug: Aborting with flag:\t" << current_flag << endl;
+	cout << "\033[1;34mDebug:\033[0m Aborting with flag:\t" << current_flag << endl;
 	flags_display();
-	if(turned_on) cout << "Debug: Terminated" <<endl;
+	if(turned_on) cout << "\033[1;34mDebug:\033[0m Terminated" <<endl;
 	exit(0);
       }
     }
   }
   void abort(){
     if(turned_on){
-	cout << "Debug: Aborting. Absolute." << endl;
+	cout << "\033[1;34mDebug:\033[0m Aborting. Absolute." << endl;
 	flags_display();
-	if(turned_on) cout << "Debug: Terminated" <<endl;
+	if(turned_on) cout << "\033[1;34mDebug:\033[0m Terminated" <<endl;
 	exit(0);
     }
   }
   void pause(){
     if(turned_on){
-      cout << "Debug: Paused. Press Enter.";
+      cout << "\033[1;34mDebug:\033[0m Paused. Press Enter.";
       cin.get();
     }
   }
