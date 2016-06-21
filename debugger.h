b9e1072459f8461915d3e46ca5c8f28a61219765
scottcrossen <scottcrossen42@gmail.com>
2016-06-21 09:55:35 -0600

    added functionality to debug class

--- debugger.h
+++ debugger.h
@@ -1,48 +1,97 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <stdlib.h>
 using namespace std;
 
 class Debugger{
  private:
   vector<int> flags;
- public:
   bool turned_on;
-  bool flags_on;
+  bool last_flag;
+ public:
   Debugger(bool turned_on){
     this->turned_on=turned_on;
-    flags_on=false;
+    last_flag=false;
     if(turned_on) cout << "Debug: Initialized" <<endl;
   }
   ~Debugger(){
     if(turned_on) cout << "Debug: Terminated" <<endl;
   }
   void flag(int add_flag){
-    flags.push_back(add_flag);
-    if(turned_on && flags_on) cout << "Debug: Flag "<< add_flag << " reached" <<endl;
+    if(turned_on){
+      flags.push_back(add_flag);
+      if(!(last_flag)) cout << "Debug: Flags encountered:\t"<< add_flag;
+      else cout << " " << add_flag;
+      last_flag=true;
+    }
   }
   void flags_display(){
     if(turned_on){
-      cout << "Debug: Flags encountered:\t";
+      if(last_flag){ cout << endl; last_flag=false;}
+      cout << "Debug: Displaying flags found:\t";
       for(unsigned i=0; i < flags.size(); i++)
 	cout << flags[i] << " ";
       cout << endl;
     }
   }
   void output(string current_output){
-    if(turned_on) cout << "Debug: Status line:\t\t" << current_output << endl;
+    if(turned_on){
+      if(last_flag){cout << endl; last_flag=false;}
+      cout << "Debug: Status line:\t\t" << current_output << endl;
+    }
   }
   void flags_clear(){
-    for (unsigned i=0; i < flags.size(); i++) flags.pop_back();
-    if(turned_on) cout << "Debug: Flags cleared." << endl;
+    if(turned_on){
+      if(last_flag){ cout << endl; last_flag=false;}
+      for (unsigned i=0; i < flags.size(); i++) flags.pop_back();
+      cout << "Debug: Flags cleared." << endl;
+    }
   }
   bool encountered(int current_flag){
-    for (unsigned i=0; i < flags.size(); i++)
-      if(flags[i]==current_flag){
-	if(turned_on) cout << "Debug: Flag 1 conditional met:\tTrue"<< endl;
-	return true;
+    if(turned_on){
+      if(last_flag){ cout << endl; last_flag=false;}
+      for (unsigned i=0; i < flags.size(); i++)
+	if(flags[i]==current_flag){
+	  cout << "Debug: Flag 1 conditional met:\tTrue"<< endl;
+	  return true;
+	}
+      cout << "Debug: Flag 1 conditional met:\tFalse"<< endl;
+      return false;
+    } else return false;
+  }
+  void abort(bool conditional_met){
+    if(turned_on){
+      if(conditional_met){
+	cout << "Debug: Aborting. Unknown flag." << endl;
+	flags_display();
+	if(turned_on) cout << "Debug: Terminated" <<endl;
+	exit(0);
       }
-    if(turned_on) cout << "Debug: Flag 1 conditional met:\tFalse"<< endl;
-    return false;
+    }
+  }
+  void abort(bool conditional_met, int current_flag){
+    if(turned_on){
+      if(conditional_met){
+	cout << "Debug: Aborting with flag:\t" << current_flag << endl;
+	flags_display();
+	if(turned_on) cout << "Debug: Terminated" <<endl;
+	exit(0);
+      }
+    }
+  }
+  void abort(){
+    if(turned_on){
+	cout << "Debug: Aborting. Absolute." << endl;
+	flags_display();
+	if(turned_on) cout << "Debug: Terminated" <<endl;
+	exit(0);
+    }
+  }
+  void pause(){
+    if(turned_on){
+      cout << "Debug: Paused. Press Enter.";
+      cin.get();
+    }
   }
 };
--- test.cpp
+++ test.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include "debugger.h"
-using namespace std;
+
 int main(){
   Debugger debug=Debugger(true);
-  debug.flags_on=true;
   debug.output("Debugger class seems to be working.");
   debug.flag(1);
+  debug.flag(2);
   debug.flags_display();
   if(!(debug.encountered(1))) debug.flag(1);
   debug.flags_clear();
+  debug.abort();
 }
