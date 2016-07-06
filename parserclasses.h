9570efd3ea38c0cb1285f942cc4d476dac6d1843
scottcrossen <scottcrossen42@gmail.com>
2016-07-06 15:24:37 -0600

    created actual parserclasses

--- parserclasses.h
+++ parserclasses.h
@@ -1,58 +1,108 @@
-#include "programobject.h"
 #pragma once
 
-class DatalogProgram {
+class Parameter {
  public:
-  DatalogProgram(){}
-  DatalogProgram(ProgramObject object){
-    this->object=object;
+  Parameter(){}
+  Parameter(string ID){
+    this->ID=ID;
   }
-  ~DatalogProgram(){}
+  ~Parameter(){}
   string toString(){
-    return object.to_string();
+    return ID;
   }
  private:
-  ProgramObject object;
-};
-
-class Rule {
- public:
-  Rule(){}
-  Rule(ProgramObject object){
-    this->object=object;
-  }
-  ~Rule(){}
-  string toString(){
-    return object.to_string();
-  }
- private:
-  ProgramObject object;
+  string ID;
 };
 
 class Predicate {
  public:
   Predicate(){}
-  Predicate(ProgramObject object){
-    this->object=object;
+  Predicate(Parameter ID, vector<Parameter> parameter_list){
+    this->ID=ID;
+    this->parameter_list=parameter_list;
+  }
+  Predicate(Parameter ID){
+    this->ID=ID;
+  }
+  void add_parameter(Parameter parameter){
+    parameter_list.push_back(parameter);
   }
   ~Predicate(){}
   string toString(){
-    return object.to_string();
+    stringstream output;
+    output << ID.toString() << "(";
+    for(unsigned iter=0; iter < parameter_list.size()-1; iter++)
+      output << parameter_list[iter].toString() << ",";
+    output << parameter_list[parameter_list.size()-1].toString() << ")";
+    return output.str();
   }
  private:
-  ProgramObject object;
+  Parameter ID;
+  vector<Parameter> parameter_list;
 };
 
-class Parameter {
+class Rule {
  public:
-  Parameter(){}
-  Parameter(ProgramObject object){
-    this->object=object;
+  Rule(){};
+  Rule(Predicate rule, vector<Predicate> predicates){
+    this->rule=rule;
+    this->predicates=predicates;
   }
-  ~Parameter(){}
+  Rule(Predicate rule){
+    this->rule=rule;
+  }
+  void add_predicate(Parameter predicate){
+    predicates.push_back(predicate);
+  }
+  ~Rule(){}
   string toString(){
-    return object.to_string();
+    stringstream output;
+    output << rule.toString() << " :- ";
+    for(unsigned iter=0; iter <predicates.size()-1; iter++)
+      output << predicates[iter].toString() <<",";
+    output << predicates[predicates.size()-1].toString();
+    return output.str();
   }
  private:
-  ProgramObject object;
+  Predicate rule;
+  vector<Predicate> predicates;
+};
+
+class DatalogProgram {
+ public:
+  DatalogProgram(){}
+  ~DatalogProgram(){}
+  string toString(){
+    stringstream output;
+    output << "Schemes(" << schemes.size() <<"):"<<endl;
+    for(unsigned iter=0; iter < schemes.size(); iter++)
+      output <<"  "<< schemes[iter].toString()<<endl;
+    output << "Facts(" << schemes.size() <<"):"<<endl;
+    for(unsigned iter=0; iter < schemes.size(); iter++)
+      output <<"  "<< schemes[iter].toString()<<endl;
+    output << "Rules(" << schemes.size() <<"):"<<endl;
+    for(unsigned iter=0; iter < schemes.size(); iter++)
+      output <<"  "<< schemes[iter].toString()<<endl;
+    output << "Queries(" << schemes.size() <<"):"<<endl;
+    for(unsigned iter=0; iter < schemes.size(); iter++)
+      output <<"  "<< schemes[iter].toString()<<endl;
+    return output.str();
+  }
+  void add_scheme(Predicate scheme){
+    schemes.push_back(scheme);
+  }
+  void add_fact(Predicate fact){
+    facts.push_back(fact);
+  }
+  void add_rule(Rule rule){
+    rules.push_back(rule);
+  }
+  void add_query(Predicate query){
+    queries.push_back(query);
+  }
+ private:
+  vector<Predicate> schemes;
+  vector<Predicate> facts;
+  vector<Rule> rules;
+  vector<Predicate> queries;
 };
