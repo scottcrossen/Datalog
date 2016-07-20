aad8978bdd13cf3797b044760a175c859a6e9bca
scottcrossen <scottcrossen42@gmail.com>
2016-07-20 15:06:16 -0600

    made operators on relations

--- databaseclasses.h
+++ databaseclasses.h
@@ -85,6 +85,81 @@ class RelationNode{
     if(this->node== second.node) return true;
     else return false;
   }
+  void operator= (const RelationNode& second){
+    this->node=second.node;
+    this->columns=second.columns;
+    this->tuples=tuples;
+  }
+  RelationNode operatorU (const RelationNode& second){
+    debug.abort(3, this->columns != second.columns);
+    RelationNode output=(*this);
+    for(set<vector<Parameter>>::iterator iter1=second.tuples.begin(); iter1 !=second.tuples.end(); iter1++)
+      output.tuples.insert(*iter1);
+    return output;
+  }
+  RelationNode operatorX (const RelationNode& second){
+    RelationNode output;
+    output.node=string();
+    vector<QueryParam*> new_columns;
+    vector<QueryParam*> first_columns;
+    vector<QueryParam*> second_columns;
+    // First create the vectors that should match to make a new tuple
+    for(vector<Parameter>::iterator iter1=this->columns.begin(); iter1 !=this->columns.begin(); iter1++){
+      bool found_match=false;
+      for(vector<QueryParam*>::iterator iter2=new_columns.begin(); iter2 != new_culmns.end();iter2++)
+	if ((*iter1).return_ID() == (*iter2)->return_natural()){
+	  found_match=true;
+	  first_columns.push_back(*iter2);
+	  break;
+	}
+      if (!(found_match)){
+	QueryParam* new_queryparam=new QueryParam((*iter1).return_ID());
+	new_columns.push_back(new_queryparam);
+	first_columns.push_back(new_queryparam);
+      }
+    }
+    for(vector<Parameter>::iterator iter1=second.columns.begin(); iter1 !=second.columns.begin(); iter1++){
+      bool found_match=false;
+      for(vector<QueryParam*>::iterator iter2=new_columns.begin(); iter2 != new_culmns.end();iter2++)
+	if ((*iter1).return_ID() == (*iter2)->return_natural()){
+	  found_match=true;
+	  second_columns.push_back(*iter2);
+	  break;
+	}
+      if (!(found_match)){
+	QueryParam* new_queryparam=new QueryParam((*iter1).return_ID());
+	new_columns.push_back(new_queryparam);
+	second_columns.push_back(new_queryparam);
+      }
+    }
+    // Iterate through set and add matching tuples.
+    for(set<vector<Parameter>>::iterator iter1=this->tuples.begin();iter1 !=this->tuples.end();iter1++)
+      for(set<vector<Parameter>>::iterator iter2=second.tuples.begin();iter2 !=second.tuples.end();iter2++){
+	for(vector<QueryParam>::iterator iter3=new_columns.begin(); iter3 !=new_columns.end(); iter3++)
+	  iter3->reset();
+	bool matches=true;
+	for(unsigned iter3=0;iter3<first_columns.size();iter3++){
+	  matches=first_columns[iter3]->compare((*iter1)[iter3]);
+	  if(!(matches)) break;
+	}
+	if(matches)
+	for(unsigned iter3=0;iter3<second_columns.size();iter3++){
+	  matches=second_columns[iter3]->compare((*iter2)[iter3]);
+	  if(!(matches)) break;
+	}
+	if(matches){
+	  vector<Parameter> add_tuple;
+	  for(vector<QueryParam>::iterator iter3=new_columns.begin(); iter3 !=new_columns.end(); iter3++)
+	    add_tuple.push_back(Parameter(iter3->return_value()));
+	  output.tuples.insert(add_tuple);
+	}
+      }
+    for(vector<QueryParam>::iterator iter1=new_columns.begin(); iter1 !=new_columns.end(); iter1++){
+      output.columns.push_back(Parameter(QueryParam(iter1->return_natural())));
+      delete (*iter1);
+    }
+    return output;
+  }
   void debug_on(bool turn_on) const{
     debug.turned_on=turn_on;
     debug.output("Debug turned on.");
